5724e7290c4c0e2a157ce3840c46217a98c30aac
showkhin1997 <showkhin15@gmail.com>
2023-05-28 19:02:34 +0600

    first commit

---
+++ string-search.js
@@ -0,0 +1,22 @@
+const products = [
+    "asus zenbook laptop",
+    "samsung Laptop",
+    "LG phone with Laptop",
+    "gaming monitor from MSI",
+    "tenda router with gaming PC",
+    "Gamidas gaming Mouse",
+    "Fantech mouse",
+    "rapoo mechanical keyboard",
+    "Fantech gaming RGB keyboard"
+];
+
+const searchingInput = "gaming";
+const output = [];
+
+for (const product of products) {
+    if (product.toLowerCase().indexOf(searchingInput.toLowerCase()) != -1) {
+        output.push(product);
+    }
+}
+
+console.log(output);
